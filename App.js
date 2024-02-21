62c9efb2e06c48c68be151cf0478a809dbc8a808
ChaeDoll <dlacodbs0627@gmail.com>
2024-02-21 23:29:02 +0900

    restart

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { Dimensions } from 'react-native';
-import { router } from 'expo-router';
-
-const { width: SCREEN_WIDTH } = Dimensions.get('winow');
+import { Dimensions, View, Text } from 'react-native';
+// const { width: SCREEN_WIDTH } = Dimensions.get('winow');
 export default function App() {
   return (
       <View>
-
+        <Text>하이</Text>
       </View>
   );
 }
