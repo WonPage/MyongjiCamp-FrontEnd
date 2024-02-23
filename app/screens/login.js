b9d4661e43f433c617ff0635386f862f26e35c00
Jang-SoHyeon <jangso6593@gmail.com>
2024-02-23 20:48:14 +0900

    240223

--- app/screens/login.js
+++ app/screens/login.js
@@ -1,177 +1,240 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { Link } from "expo-router";
 
-import { KeyboardAvoidingView,Platform,StyleSheet, View,TextInput, Button,Text,Image,Switch, Alert } from "react-native";
+import { TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, StyleSheet, View, TextInput, Button, Text, Image, Switch, Alert, TouchableOpacity } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 // 드래그 & ctrl alt l -> 자동 정렬
+// 학교 하늘 색 #008FD5, 남색 #002E66
 
-export default function Login({navigation}){
+export default function Login({ navigation }) {
     const [emailPrefix, setEmailPrefix] = useState('');
     const [password, setPassword] = useState('');
     const [stayLoggedIn, setStayLoggedIn] = useState(false);
 
     // 로그인 버튼 눌렀을 때
-    const handleConfirm = async()=>{
+    const handleConfirm = async () => {
         const fullEmail = `${emailPrefix}@mju.ac.kr`;
-        console.log(JSON.stringify({email:fullEmail, password: password}))
+        // console.log(JSON.stringify({ email: fullEmail, password: password }))
 
-/*         try{
-            const response = await fetch('api_url_써야돼',{
-                method: 'POST',
-                headers: {
-                    'Content-Type' : 'application/json'
-                },
-                body : JSON.stringify({email:fullEmail, password: password}),
-            });
+        /*         try{
+                    const response = await fetch('api_url_써야돼',{
+                        method: 'POST',
+                        headers: {
+                            'Content-Type' : 'application/json'
+                        },
+                        body : JSON.stringify({email:fullEmail, password: password}),
+                    });
+                    const result = await response.json(); //백으로 받은 json 응답
 
-            const result = await response.json(); //백으로 받은 json 응답
+        
+                    if(response.ok && result.loginSuccess){
+                        Alert.alert('로그인 성공', '나 확인용(지울거)');
+                        // 메인 화면 열어야 됨
+                    }
+                    else{
+                        Alert.alert('로그인 실패','이메일 또는 비밀번호가 잘못되었습니다.');
+                        setPassword('');
 
-            if(response.ok){
-                Alert.alert('성공', '이메일이 성공적으로 전송되었습니다.');
-            }
-            else{
-                Alert.alert('실패','전송 실패');
-            }
-        }
-        catch(error){
-            Alert.alert('오류','네트워크 오류');
-        }; */
+                    }
+                    
+                    
+                    
+
+
+                }
+                catch(error){
+                    Alert.alert('오류','네트워크 오류가 발생하였습니다.');
+                }; */
     }
-    
- 
+
+    // 비밀번호 찾기 버튼 눌렀을 때
+    const handleFindPassword = () => {
+        Alert.alert('비밀번호를 찾습니다');
+    }
+
+    // 앱 둘러보기 버튼 눌렀을 때
+    const handleAppTour=()=>{
+        Alert.alert('보자보자')
+    }
+
     //이메일 콘솔에 뭐라고 찍히는지 확인하기
     return (
-        <KeyboardAvoidingView 
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style ={styles.container}>
-            <View style = {styles.con}>
-            <View style ={styles.icon}>
-            <Image 
-            style={styles.myongji_icon}
-            source={require('../../assets/myongji-icon.png')}/>
-            </View>
-            <View style ={styles.input}>
-                <View style = {styles.input_box}>
-                    <TextInput 
-                    style={styles.input_email_txt}
-                    placeholder="이메일"
-                    value={emailPrefix}
-                    onChangeText={setEmailPrefix}
-                    autoCapitalize='none'
-                    maxLength={20} // @앞의 최대 글자 수 30자
-                    />
-                    <Text style={styles.input_email_rear}>@mju.ac.kr</Text>
-                </View>
-                <TextInput
-                style={styles.input_password}
-                placeholder="비밀번호"
-                value={password}
-                secureTextEntry
-                onChangeText={setPassword}
-                autoCapitalize='none'
-                maxLength={20} // 최대 글자 수 30자
-                />
-            </View>
-            <View style={styles.checkBox_container}>
-                <View style={styles.checkBox}>
-                    <View style={styles.ex}>
-                    <BouncyCheckbox
-                    size={20}
-                    textStyle={{
-                        textDecorationLine : "none"
-                    }}
-                    text="로그인 유지"
-                    />
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.container}>
+                <View style={styles.container_width}>
+                    <View style={styles.icon}>
+                        <Image
+                            style={styles.myongji_icon}
+                            source={require('../../assets/myongji-icon.png')} />
+                    </View>
+                    <View style={styles.input}>
+                        <View style={styles.input_box}>
+                            <TextInput
+                                style={styles.input_email_txt}
+                                placeholder="이메일"
+                                value={emailPrefix}
+                                onChangeText={setEmailPrefix}
+                                autoCapitalize='none'
+                                maxLength={20} // @앞의 최대 글자 수 30자
+                            />
+                            <Text style={styles.input_email_rear}>@mju.ac.kr</Text>
+                        </View>
+                        <TextInput
+                            style={styles.input_password}
+                            placeholder="비밀번호"
+                            value={password}
+                            secureTextEntry
+                            onChangeText={setPassword}
+                            autoCapitalize='none'
+                            maxLength={20} // 최대 글자 수 30자
+                        />
+                        <View style={styles.checkBox}>
+                            <BouncyCheckbox
+                                size={20}
+                                textStyle={{
+                                    textDecorationLine: "none"
+                                }}
+                                fillColor='#008FD5'
+                                text="로그인 유지"
+                            />
+                        </View>
+                    </View>
+                    {/* <View style={styles.checkBox_container}> */}
+{/* 여기여기 */}
+                    {/* </View> */}
+                    <View style={styles.confirm}>
+                    <TouchableOpacity
+                            style={styles.confirm_button}
+                            onPress={handleConfirm}>
+                            <Text style={styles.confirm_button_txt}>로그인</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.cant}>
+                        <TouchableOpacity
+                        style={styles.cant_findPassword_button}
+                            onPress={handleFindPassword}>
+                            <Text style={styles.cant_findPassword_button_txt}>비밀번호 찾기</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                        style={styles.cant_singup_button}
+                            onPress={() => navigation.navigate('Step1')}>
+                            <Text style={styles.cant_singup_button_txt}>회원가입</Text>
+                        </TouchableOpacity>
+
+                    </View>
+
+                    <View style={styles.app_tour}>
+
+                    <TouchableOpacity
+                    style={styles.app_tour_button}
+                            onPress={handleAppTour}>
+                            <Text>앱 둘러보기</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
-            </View>
-            <View style={styles.confirm_container}>
-                <Button
-                style = {styles.confirm}
-                title = "로그인"
-                onPress={handleConfirm}
-                />
-            </View>
-            <View style={styles.cant}>
-                <Button
-                title = "회원가입"
-                onPress={()=>navigation.navigate('Step1')}
-                />
-            </View>
-            <View style={styles.app_tour}></View>
-            </View>
-        </KeyboardAvoidingView>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     );
 }
 const styles = StyleSheet.create({
-    container : {
-        flex : 1,
-        alignItems:"center",
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent : "center"
     },
-    con : {
-        flex:1,
-        width : "80%",
+    container_width: {
+        width: "75%",
+        height: '80%',
     },
-    icon : {
-        flex : 3,
+    icon: {
+        flex: 3,
         justifyContent: 'center',
-        alignItems: 'center'
-    },
-    myongji_icon : {
-        height: 100,
-        width: 100
-    },
-    input : {
-        flex : 2.5,
-    },
-    input_box : {
-        flexDirection: 'row', 
         alignItems: 'center',
-        justifyContent:'space-around',
-        borderWidth: 1,
-        height : 40,
-        marginVertical : 10,
-        borderRadius:15,
-        paddingHorizontal : 10,
-    },
-    input_email_txt : {
-        fontSize : 15,
-    },
-    input_email_rear : {
-        fontSize : 15,
-        color : 'gray',
-        width : '40%',
-    },
-    input_password : {
-        borderWidth: 1,
-        alignItems : 'center',
-        height : 40,
-        borderRadius:15,
-        paddingHorizontal : 10,
-        fontSize : 15
-    },
-    checkBox_container : {
-        flex : 0.5,
-    },
-    checkBox : {
-        borderRadius : 20,
 
     },
-    confirm_container : {
-        flex : 1.5,
-        backgroundColor : 'red'
+    myongji_icon: {
+        height: 100,
+        width: 100,
     },
-    confirm:{
-        backgroundColor:"#1e90ff",
-        borderRadius : 20
+    input: {
+        flex: 2.5,
+        justifyContent : "space-around"
     },
-    cant : {
+    input_box: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        height: 50,
+        borderRadius: 16,
+        paddingHorizontal: 20,
+    },
+    input_email_txt: {
+        flex :1,
+        fontSize: 15,
+    },
+    input_email_rear: {
+        fontSize: 15,
+        color: 'gray',
+    },
+    input_password: {
+        borderWidth: 1,
+        alignItems: 'center',
+        height: 50,
+        borderRadius: 15,
+        paddingHorizontal: 20,
+        fontSize: 15
+    },
+    checkBox: {
+        width: "40%",
+        borderRadius: 20,
+        justifyContent: 'flex-end',
+    },
+    confirm: {
+        flex: 1,
+        justifyContent:"center",
+        // backgroundColor:"red"
+    },
+    confirm_button: {
+        borderRadius: 20,
+        height : 60,
+        backgroundColor: "#002E66",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    confirm_button_txt:{
+        fontSize : 23,
+        color : "white",
+    },
+    cant: {
+        flex: 0.5,
+        flexDirection: 'row',
+    },
+    cant_findPassword_button :{
         flex : 0.5,
-        backgroundColor : 'green'
+        justifyContent : "center",
+        alignItems : "center"
+
     },
-    app_tour : {
-        flex : 2,
-        backgroundColor : 'white'
+    cant_findPassword_button_txt : {
+        fontSize:15,
+
+    },
+    cant_singup_button : {
+        flex : 0.5,
+        justifyContent : "center",
+        alignItems : "center"
+    },
+    cant_singup_button_txt : {
+        fontSize:15,
+    },
+    app_tour: {
+        flex: 2,
+        backgroundColor : "red",
+    },
+    app_tour_button : {
+
     }
 
 });
