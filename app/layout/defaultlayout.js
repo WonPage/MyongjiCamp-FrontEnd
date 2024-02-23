f66ebb240aee123f97ba1512db3d7a169b904c96
ChaeDoll <dlacodbs0627@gmail.com>
2024-02-24 00:36:23 +0900

    chae merge

--- app/layout/defaultlayout.js
+++ app/layout/defaultlayout.js
@@ -1,15 +1,11 @@
 import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, TouchableWithoutFeedback } from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 
 /** 키보드가 View를 방해하지 않고, 키보드 밖을 터치하면 키보드가 해제됨 */
 export default function DefaultLayout({children}){
     return (
-
         <KeyboardAvoidingView style={styles.layout}
-            behavior={
-                
-                Platform.OS === "ios" ? 'padding' : 'height'}>
+            behavior={Platform.OS === "ios" ? 'padding' : 'height'}>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                 {children}
             </TouchableWithoutFeedback>
@@ -19,7 +15,6 @@ export default function DefaultLayout({children}){
 
 const styles = StyleSheet.create({
     layout: {
-        flex: 1,
-        justifyContent : "center",
+        flex: 1
     }
 })
